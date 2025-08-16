746f2001cde909cc906398e31e6e7c096a93e61f
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-16 08:23:25 +0330

    feat: add Header and Footer components with navigation
    - Implemented Header component with logo and TopNavigation.
    - Created TopNavigation component for main navigation links.
    - Added Footer component with branding and additional links.
    - Updated layout to include Header and Footer, replacing placeholder content.
    - Defined NavigationMenuItem type for better type safety in navigation.

---
+++ src/app/_components/header/Header.tsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import React from "react";
+import TopNavigation from "./TopNavigation";
+
+const Header: React.FC = () => {
+  return (
+    <header className="border-b dark:border-base-content dark:border-opacity-5">
+      <div className="container flex items-center justify-between">
+        <Image
+          src="/images/logo-light.svg"
+          width={100}
+          height={36}
+          alt="کلاسبن"
+        />
+        <TopNavigation />
+        <div className="mr-auto">User Authentication</div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import Footer from "./_components/footer/Footer";
+import Header from "./_components/header/Header";
 import "./globals.css";
 
 export default function RootLayout({
@@ -6,17 +8,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl">
-      <body className="flex flex-col min-h-screen font-bold uppercase">
-        <header className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          HEADER
-        </header>
+    <html lang="en" dir="rtl" className="dark">
+      <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
+        <Header />
         <div className="flex-1 flex  justify-center items-center">
           {children}
         </div>
-        <footer className="bg-gray-200 flex items-center justify-center text-3xl h-20">
-          FOOTER
-        </footer>
+        <Footer />
       </body>
     </html>
   );
