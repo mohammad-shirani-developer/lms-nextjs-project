1d0bf4b1bbb2e6f918e32ade6ce9782ed2613201
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-16 10:38:45 +0330

    feat: add custom fonts and update layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,47 @@
+import { Figtree } from "next/font/google";
+import localFont from "next/font/local";
 import Footer from "./_components/footer/Footer";
 import Header from "./_components/header/Header";
 import "./globals.css";
+
+const figtree = Figtree({
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--font-figtree",
+  weight: ["300", "400", "500", "600", "700", "700", "800", "900"],
+});
+
+const iranyekan = localFont({
+  src: [
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Light.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Regular.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-DemiBold.woff2",
+      weight: "600",
+      style: "normal",
+    },
+    {
+      path: "../../public/fonts/iranyekan/IRANYekanXFaNum-Bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+  ],
+  variable: "--font-iranyekan",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -8,7 +49,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" dir="rtl" className="dark">
+    <html
+      dir="rtl"
+      className={`dark ${figtree.variable} ${iranyekan.variable}`}
+    >
       <body className="min-h-screen grid grid-rows-[80px_1fr_auto] dark:bg-base-100 dark:text-base-content">
         <Header />
         <div className="flex-1 flex  justify-center items-center">
