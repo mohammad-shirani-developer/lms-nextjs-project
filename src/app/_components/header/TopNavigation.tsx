26b61df1b6acc11174ebdc25b9cefebf8f66de26
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-20 15:49:08 +0330

    fix: correct import path for NavigationMenuItem in TopNavigation component

--- src/app/_components/header/TopNavigation.tsx
+++ src/app/_components/header/TopNavigation.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { NavigationMenuItem } from "@/app/types/navigation-menu-item";
+import { NavigationMenuItem } from "@/types/navigation-menu-item";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import React from "react";
