719bcb48452bb4f1ee335d7bfefd2d00dc155aa8
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-21 11:12:16 +0330

    feat: Add ReactJs & Next.js section with buttons and content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import HomeHeroSection from "./_components/homeHeroSection/HomeHeroSection";
 import CourseCardList from "./(courses)/_components/CourseCardList";
 import { homeFeatures } from "@/data/home-features";
 import { Feature } from "./_components/feature";
+import { Button } from "./_components/button";
+import { IconArrowLeftFill } from "./_components/icons/icons";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -37,6 +39,44 @@ export default async function Home() {
           </p>
         </div>
         <CourseCardList courses={newestCourses} />
+      </section>
+      <section className="px-2 my-40">
+        {/* <div className="sticky top-0 pt-0 text-center"> */}
+        <div className="relative pt-0 text-center">
+          <div className="bg-primary pointer-events-none absolute left-1/2 aspect-square w-1/2 -translate-x-1/2 -top-96 rounded-full opacity-10 blur-3xl"></div>
+
+          <h2
+            lang="en"
+            className="gradient leading-[1.3] relative z-10 mx-auto inline-block text-[clamp(2rem,6vw,5.5rem)] font-black"
+          >
+            ReactJs & Next.js
+          </h2>
+          <p className="text-base-content/70  relative z-[2] py-4 m-auto md:text-3xl max-w-5xl font-light !leading-[1.7]">
+            ری‌اکت و نکست‌جی‌اس برترین کتابخونه‌های فرانت‌اند و یکه‌تاز دنیای
+            وب! پیشرفته‌ترین مباحث رو اینجا می تونی پیدا کنی. پس همین الان
+            یادگیری رو شروع کن ما هم از ابتدای مسیر با آموزش‌های تخصصی و کاملاً
+            کاربردی کنارت هستیم.
+          </p>
+          <div className="flex flex-col lg:flex-row items-center gap-3 justify-center">
+            <Button
+              variant="primary"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              دوره‌های ری اکت و نکست‌ جی‌اس
+              <IconArrowLeftFill fill="currentColor" />
+            </Button>
+            <Button
+              variant="neutral"
+              size="large"
+              className="mt-7"
+              animatedIcon={true}
+            >
+              مقالات ری اکت و نکست‌ جی‌اس
+            </Button>
+          </div>
+        </div>
       </section>
     </>
   );
