12e0cf0d2dd3ad7c8ce8e6849381bc2a728b472c
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-21 11:30:27 +0330

    feat: Integrate BlogPostSummary interface and fetch newest blog posts in Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import { homeFeatures } from "@/data/home-features";
 import { Feature } from "./_components/feature";
 import { Button } from "./_components/button";
 import { IconArrowLeftFill } from "./_components/icons/icons";
+import { BlogPostSummary } from "@/types/blog-post-summary.interface";
 
 async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   const res = await fetch(
@@ -17,9 +18,23 @@ async function getNewestCourses(count: number): Promise<CourseSummary[]> {
   );
   return res.json();
 }
+async function getNewestPosts(count: number): Promise<BlogPostSummary[]> {
+  const res = await fetch(`https://api.classbon.com/api/blog/newest/${count}`, {
+    next: {
+      revalidate: 24 * 60 * 60,
+    },
+  });
+  return res.json();
+}
 
 export default async function Home() {
-  const newestCourses = await getNewestCourses(4);
+  const newestCoursesData = getNewestCourses(4);
+  const newestBlogPostsData = getNewestPosts(4);
+  const [newestCourses, newestBlogPosts] = await Promise.all([
+    newestCoursesData,
+    newestBlogPostsData,
+  ]);
+  console.log(newestBlogPosts[0].title);
 
   return (
     <>
