0bb8a105526ec3555c531508c1cf2d2e4d19d48d
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-23 09:19:29 +0330

    feat: Implement CourseDetails page with dynamic course fetching and layout

--- src/app/(courses)/courses/[slug]/page.tsx
+++ src/app/(courses)/courses/[slug]/page.tsx
@@ -1,7 +1,43 @@
-export default async function CourseDetails() {
+import { API_URL } from "@/configs/globals";
+import type { CourseDetails } from "@/types/course-details.interface";
+
+export async function generateStaticParams() {
+  const slugs = await fetch(`${API_URL}/courses/slugs`).then((res) =>
+    res.json()
+  );
+
+  return (slugs as string[]).map((slug) => ({
+    slug: slug,
+  }));
+}
+
+async function getCourse(slug: string): Promise<CourseDetails> {
+  const res = await fetch(`${API_URL}/courses/${slug}`);
+  return res.json();
+}
+export default async function CourseDetails({
+  params,
+}: {
+  params: { slug: string };
+}) {
+  const { slug } = params;
+  const course = await getCourse(slug);
   return (
-    <div className="text-5xl flex justify-center items-center w-full">
-      <h1>This is course details page</h1>
+    <div className="h-96 container grid grid-cols-10 grid-rows-[1fr 1fr] gap-10 py-10">
+      <div className="bg-primary pointer-events-none absolute right-0 aspect-square w-1/2   rounded-full opacity-10 blur-3xl"></div>
+      <div className=" col-span-10 xl:col-span-7 ">
+        <h1 className="text-center xl:text-right text-2xl lg:text-3xl xl:text-4xl font-black leading-10">
+          {course.title}
+        </h1>
+        <h2 className="mt-4 text-center xl:text-right text-lg  leading-9">
+          {course.subTitle}
+        </h2>
+
+        <div className=" mt-5">Video Player Component</div>
+      </div>
+      <div className=" col-span-10 xl:col-span-3 bg-secondary"></div>
+      <div className=" col-span-10 xl:col-span-6 bg-info"></div>
+      <div className=" col-span-10 xl:col-span-4 bg-warning"></div>
     </div>
   );
 }
