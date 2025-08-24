0e8a2ad517bdd3ec70b0774a1c2aa098653dc52c
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-24 09:19:53 +0330

    feat: Creat new provider with reactQuery

---
+++ src/providers/ReactQueryProvider.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { queryClient } from "@/lib/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { PropsWithChildren } from "react";
+
+function QueryProvider({ children }: PropsWithChildren) {
+  return (
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
+}
+
+export default QueryProvider;
