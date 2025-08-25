09d7adc2dc723e80fdbf17feaf81b056c3c857f6
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-25 14:35:04 +0330

    feat: Implement VideoPlayer Component

--- src/app/_components/loading/loading.types.ts
+++ src/app/_components/loading/loading.types.ts
@@ -1,5 +1,5 @@
 import { ComponentBase } from "../types/component-base.type";
 
-export type LoadingProps=Omit<ComponentBase,"isDisabled">&{
-    type: "spinner" | "ring";
-}
+export type LoadingProps = Omit<ComponentBase, "isDisabled"> & {
+  type?: "spinner" | "ring";
+};
