a5e42f007a4bccfcf2d88de0433e3778ec85f937
Mohammad Shirani <mohammad.shirani.developer@gmail.com>
2025-08-25 15:16:47 +0330

    feat: Adding auth template

---
+++ src/app/(auth)/signin/page.tsx
@@ -0,0 +1,8 @@
+import Link from "next/link";
+
+export default async function SignIn() {
+    return   <div>
+  
+    <Link href="/verify">Go To Verification Page</Link>
+    </div>;
+}
---
+++ src/app/(auth)/verify/page.tsx
@@ -0,0 +1,7 @@
+import Link from "next/link";
+
+export default function Verify() {
+    return  <div className="">
+    <Link href="/signin">Go To Signin Page</Link>
+    </div>
+}
